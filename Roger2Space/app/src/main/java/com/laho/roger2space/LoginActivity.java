6469b8b01320f037be7b1d35afa8bb911ffb9606
Charly Berthet <charlyberthet94@gmail.com>
2015-04-11 15:40:41 +0200

    push to push

---
+++ Roger2Space/app/src/main/java/com/laho/roger2space/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.laho.roger2space;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class LoginActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
