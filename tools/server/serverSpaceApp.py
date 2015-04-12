34dace0465de4621d49365197c78eb0ca84975d3
Lightjohn <johnleger26@gmail.com>
2015-04-12 14:43:32 +0200

    full space now

--- tools/server/serverSpaceApp.py
+++ tools/server/serverSpaceApp.py
@@ -23,38 +23,38 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 
     def write_stat(self):
         f = open("data","w")
-	for i in self.liste:
-	    try:
-	        f.write(i+" "+ str(self.most_common(self.summary[i])))
-	    except:
-		f.write(i+" -1")
-	    f.write("\n")
-	f.write("STS-41-Launch-Onboard-Nat-Sound.mp3 ")
-	for i,w in enumerate(self.liste2):
+        for i in self.liste:
+            try:
+                f.write(i+" "+ str(self.most_common(self.summary[i])))
+            except:
+                f.write(i+" -1")
+            f.write("\n")
+        f.write("STS-41-Launch-Onboard-Nat-Sound.mp3 ")
+        for i,w in enumerate(self.liste2):
             try:
                 f.write(str(self.most_common(self.summary[w])))
             except:
                 f.write("-1")
             if i < len(self.liste2)-1:
                 f.write(" ")
-	f.close()
+        f.close()
 
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         data = self.data.split(" ")
-	title = data[0]
-	typem = int(data[1])
-	print "Music:",title,"Type",typem
+        title = data[0]
+        typem = int(data[1])
+        print "Music:",title,"Type",typem
         if title in self.summary:
             self.summary[title].append(typem)
-	else:
-	    self.summary[title] = [typem]
+        else:
+            self.summary[title] = [typem]
         self.write_stat()
-	
-
+        
 if __name__ == "__main__":
-    HOST, PORT = "195.154.15.21", 3000
+    #HOST, PORT = "195.154.15.21", 3000
+    HOST, PORT = "localhost", 3000
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
     # Activate the server; this will keep running until you
